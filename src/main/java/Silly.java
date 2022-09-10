a89371c3efbb3a67b3295c13a1ca515089bf2e4f
Ray <sy.hung@mail.utoronto.ca>
2022-09-10 01:18:54 -0400

    Completed Exercise 2

--- src/main/java/Silly.java
+++ src/main/java/Silly.java
@@ -73,15 +73,24 @@ public class Silly implements Comparable<Silly>{
     }
 
     /**
-     * TODO (Task 1): Create another constructor that takes in TWO parameters,
+     * (Task 1): Create another constructor that takes in TWO parameters,
      *       both of which are strings.
      *       Afterwards, set this.name to the concatenation of both
      *       strings (e.g. this.name = [first string] + [second string]).
      *       Make sure you document this method!
      */
 
-
-
+    /**
+     * Creates a new Silly object.
+     * This constructor takes in 2 strings as argument.
+     *
+     * @param s1 The first half string for this Silly object
+     * @param s2 The second half string for this Silly object
+     *
+     */
+    public Silly(String s1, String s2) {
+        this.name = s1 + s2;
+    }
 
 
     public static void main(String[] args) {
@@ -107,16 +116,17 @@ public class Silly implements Comparable<Silly>{
         Silly y = new Silly("something else");
 
         /**
-         * TODO (Task 2): Below are a bunch of calls to countStatic using
+         * (Task 2): Below are a bunch of calls to countStatic using
          *                two different instances of Silly.
          *                Fill out the expected_values array with the
          *                values that countStatic will return at each call.
          */
+
         x.countStatic();
         y.countStatic();
         x.countStatic();
         x.countStatic();
-        int[] expected_values = {};
+        int[] expected_values = {0, 1, 2, 3};
 
         System.out.println("The countStatic calls will return " + Arrays.toString(expected_values));
     }
@@ -133,7 +143,8 @@ public class Silly implements Comparable<Silly>{
      */
     @Override
     public String toString(){
-        // TODO (Task 3): Implement the body of this method!
+        // (Task 3): Implement the body of this method!
+        return name;
     }
 
     /**
@@ -149,7 +160,7 @@ public class Silly implements Comparable<Silly>{
     @Override
     public boolean equals(Object o) {
         /**
-         * TODO (Task 4): Complete the body of this method!
+         * (Task 4): Complete the body of this method!
          *                We've started it by checking the type of o for you.
          *                You just need to return true if the names are equal.
          */
@@ -161,6 +172,8 @@ public class Silly implements Comparable<Silly>{
 
         // Hint: to compare strings, we need to use .equals()
         //       e.g. s1.equals(s2)
+
+        return this.name.equals(other.name);
     }
 
     /**
@@ -186,7 +199,7 @@ public class Silly implements Comparable<Silly>{
     @Override
     public int compareTo(Silly other) {
         /**
-         * TODO (Task 5): Implement the body of this method.
+         * (Task 5): Implement the body of this method.
          *                A positive number should be returned if this.name
          *                is longer than other.name, a negative number if
          *                other.name is longer than this.name, and 0 if
@@ -194,10 +207,12 @@ public class Silly implements Comparable<Silly>{
          *                You can get the length of a string by using the
          *                .length() method.
          */
+        return this.name.length() - other.name.length();
+
     }
 
     /*
-     * TODO (Task 6): Submit the changes you made on GitHub!
+     * (Task 6): Submit the changes you made on GitHub!
      *                When you submit it, go to the 'Actions' tab. You should
      *                be able to see this exercise being autograded!
      *
